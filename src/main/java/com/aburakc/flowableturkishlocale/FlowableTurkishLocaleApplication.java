06b826df652b160d4c4d229db785770d6d409344
acostu <acostu@havelsan.com.tr>
2022-06-24 10:56:22 +0300

    init commit

---
+++ src/main/java/com/aburakc/flowableturkishlocale/FlowableTurkishLocaleApplication.java
@@ -0,0 +1,13 @@
+package com.aburakc.flowableturkishlocale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableTurkishLocaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableTurkishLocaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aburakc/flowableturkishlocale/FlowableTurkishLocaleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.aburakc.flowableturkishlocale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableTurkishLocaleApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
